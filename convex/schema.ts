9fda2f0a823563eb2042f320700ce3b74d4a0d23
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-12-23 11:35:04 -0500

    schema malinga

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,15 +1,65 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
-import { authTables } from "@convex-dev/auth/server";
-
-const applicationTables = {
-  counters: defineTable({
-    name: v.string(),
-    count: v.number(),
-  }),
-};
 
 export default defineSchema({
-  ...authTables,
-  ...applicationTables,
+  authAccounts: defineTable({
+    emailVerified: v.optional(v.string()),
+    phoneVerified: v.optional(v.string()),
+    provider: v.string(),
+    providerAccountId: v.string(),
+    secret: v.optional(v.string()),
+    userId: v.id("users"),
+  })
+    .index("providerAndAccountId", ["provider", "providerAccountId"])
+    .index("userIdAndProvider", ["userId", "provider"]),
+  authRateLimits: defineTable({
+    attemptsLeft: v.float64(),
+    identifier: v.string(),
+    lastAttemptTime: v.float64(),
+  }).index("identifier", ["identifier"]),
+  authRefreshTokens: defineTable({
+    expirationTime: v.float64(),
+    firstUsedTime: v.optional(v.float64()),
+    parentRefreshTokenId: v.optional(v.id("authRefreshTokens")),
+    sessionId: v.id("authSessions"),
+  })
+    .index("sessionId", ["sessionId"])
+    .index("sessionIdAndParentRefreshTokenId", [
+      "sessionId",
+      "parentRefreshTokenId",
+    ]),
+  authSessions: defineTable({
+    expirationTime: v.float64(),
+    userId: v.id("users"),
+  }).index("userId", ["userId"]),
+  authVerificationCodes: defineTable({
+    accountId: v.id("authAccounts"),
+    code: v.string(),
+    emailVerified: v.optional(v.string()),
+    expirationTime: v.float64(),
+    phoneVerified: v.optional(v.string()),
+    provider: v.string(),
+    verifier: v.optional(v.string()),
+  })
+    .index("accountId", ["accountId"])
+    .index("code", ["code"]),
+  authVerifiers: defineTable({
+    sessionId: v.optional(v.id("authSessions")),
+    signature: v.optional(v.string()),
+  }).index("signature", ["signature"]),
+  counter: defineTable({
+    count: v.float64(),
+    name: v.string(),
+  }),
+  users: defineTable({
+    email: v.optional(v.string()),
+    emailVerificationTime: v.optional(v.float64()),
+    image: v.optional(v.string()),
+    isAnonymous: v.optional(v.boolean()),
+    name: v.optional(v.string()),
+    phone: v.optional(v.string()),
+    phoneVerificationTime: v.optional(v.float64()),
+  })
+    .index("email", ["email"])
+    .index("phone", ["phone"]),
 });
